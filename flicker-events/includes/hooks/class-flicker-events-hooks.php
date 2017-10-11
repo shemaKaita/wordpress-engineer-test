a5ebc38d238b249f1265d3999704f560577ac34e
shema <skaitakirwa@gmail.com>
2017-10-11 22:03:34 +0200

    missed some documentation

--- flicker-events/includes/hooks/class-flicker-events-hooks.php
+++ flicker-events/includes/hooks/class-flicker-events-hooks.php
@@ -1,34 +1,58 @@
 <?php
-
+/**
+ * Store of all hooks.
+ */
 class flickerEventsHooks
 {
+    /**
+     * $actions all the actions.
+     * @var array
+     */
     private $actions = [];
+    /**
+     * $filters all the filters.
+     * @var array
+     */
     private $filters = [];
+    /**
+     * $instance static instance of the class.
+     * @var object
+     */
     protected static $instance;
     public function __construct()
     {
+        // check if instance is set.
         if (!isset($this::$instance)) {
             $this::$instance = $this;
         }
     }
-    public function add($args = [], $type = '')
+    /**
+     * add an action to the store.
+     * @param object $obj the object of the hook.
+     * @param string $type the type of hook it is.
+     */
+    public function add($obj, $type = '')
     {
         switch ($type) {
             case 'action':
-                $this::$instance->actions[] = $args;
+                $this::$instance->actions[] = $obj;
                 break;
             case 'filter':
-                $this::$instance->filters[] = $args;
+                $this::$instance->filters[] = $obj;
                 break;
         }
     }
+    /**
+     * registerHooks run all the stored hooks.
+     * @return void
+     */
     public function registerHooks()
     {
         foreach ($this::$instance->actions as $key => $action) {
             add_action($action->hook, array($action, 'run'), $action->priority);
         }
         foreach ($this::$instance->filters as $key => $filter) {
-            add_filter();
+            add_filter($action->hook, array($action, 'run'), $action->priority, $action->accepted_args);
         }
     }
 
